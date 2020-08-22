b7d9f6a0ce6f829e9be6a640603a63ea65be3a83
Tim Wanders <tim241@mailbox.org>
2020-08-22 23:50:07 +0200

    Add C, C++ & C# variants

---
+++ riesi.cs
@@ -0,0 +1,11 @@
+﻿/* dotnet script riesi.cs */
+bool   reisi = false;
+string input;
+
+Console.WriteLine($"Reisi: {reisi.ToString()}");
+Console.Write("Reisi?");
+
+input = Console.ReadLine().ToLower();
+reisi = (input == "y");
+
+Console.WriteLine($"Reisi: {reisi.ToString()}");
