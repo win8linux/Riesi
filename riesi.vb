eae5fae236927cecaa4723799e8f3a82b8d6dfb7
Kassin Dornelles <kassin.dornelles@gmail.com>
2020-08-22 09:09:29 -0300

    Update riesi.vb

--- riesi.vb
+++ riesi.vb
@@ -1,6 +1,16 @@
+Module Program
+
+Sub Main(args As String())
+
 Dim riesi As String
+riesi = "False"
 If "riesi" = True then
   Console.WriteLine("riesi")
 Else 
-  Console.WriteLine("not riesi")
+  Console.WriteLine("not riesi, press any key to continue")
+  Console.ReadKey(true)
 End if
+
+End Sub
+
+End Module
