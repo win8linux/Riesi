a2ae3039e6f383d54d363a5dcc3cf1bc2d4780a1
Julius C. Enriquez <win8linux@gmail.com>
2020-08-23 22:12:22 +0800

    riesi kotlin

---
+++ riesi.kt
@@ -0,0 +1,9 @@
+fun main(args: Array<String>) {
+    val frog = readLine()!!
+    val ribbit = frog.toLowerCase()
+    if (ribbit == "riesi") {
+        print(ribbit)
+    } else {
+        println("not riesi")
+    }
+}
