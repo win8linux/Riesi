484b553c9c45876a9b40eabed1a3cd2c5af881ef
Julius C. Enriquez <win8linux@gmail.com>
2020-08-23 23:22:01 +0800

    More riesi snek dieting

--- riesi.py
+++ riesi.py
@@ -4,11 +4,10 @@ not_riesi_ = "not " + riesi_ + "!!"
 
 def riesi(riesi):
     if riesi_ in riesi:
-        return CBLUE + riesi_ + CEND
+        return riesi_
     else:
-        return CRED + not_riesi_ + CEND
+        return not_riesi_
 
 if __name__ == "__main__":
-    print(CGREEN, frog, CEND)
     frog_input = input("> ").lower()
     print(riesi(frog_input))
